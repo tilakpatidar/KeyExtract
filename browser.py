4dc692a6cdb17484b53239a1e1caeeb6d0c20138
Tilak Patidar <tilakpatidar@gmail.com>
2015-06-23 20:51:00 +0530

    added no image load in phantomJS

--- browser.py
+++ browser.py
@@ -1,11 +1,14 @@
 from selenium import webdriver
 __driver=None
-def initBrowser():
+def initBrowser(no_css,no_images):
 	global __driver
-	__driver = webdriver.PhantomJS()
+	options = []
+	if no_images:
+		options.append('--load-images=false')
+	__driver = webdriver.PhantomJS('/usr/local/bin/phantomjs',service_args=options)
 def closeBrowser():
 	__driver.quit()
-def fetch(url,no_css,no_images):
+def fetch(url):
 	"""Return page source and dom object"""
 	global __driver
 	__driver.get(url)
